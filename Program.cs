621dd9fc4b06ce3b3fa2fd678a0ae2d9cfe8577e
Dmitriy <vanguard2207@yandex.ru>
2023-06-22 01:00:44 +0300

    Добавил первую задачу в ветку main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 
+
+*/
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+System.Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
+else System.Console.WriteLine($"Большее число: {a}, меньшее число: {b}");
+
+
