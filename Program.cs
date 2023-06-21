ba118c66abb17d081e456e409814eeb9d19b8101
Dmitriy <vanguard2207@yandex.ru>
2023-06-22 01:02:28 +0300

    Добавил третью задачу в ветку DZ2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-﻿/* Задача 2
+﻿/* Задача 6
 
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3 
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
 
 */
 
-Console.WriteLine("Введите число");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
-System.Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
-else System.Console.WriteLine($"Большее число: {a}, меньшее число: {b}");
+Console.WriteLine("Введите число:");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+            }
+        
